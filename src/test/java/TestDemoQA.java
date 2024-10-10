4a4a7cf229bc6c6c05907ba1872392582eb54858
a.reshetnikov <reshetnikov.a@south.rt.ru>
2024-10-10 22:42:34 +0300

    add TestDemoQA fix

--- src/test/java/TestDemoQA.java
+++ src/test/java/TestDemoQA.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
+import static java.lang.Thread.sleep;
 
 public class TestDemoQA {
 
@@ -38,11 +39,25 @@ public class TestDemoQA {
         $("label[for='hobbies-checkbox-2']").click();
         File file = new File("C:\\Users\\a.reshetnikov\\IdeaProjects\\lesson3\\src\\picture.png");
         $("#uploadPicture").uploadFile(file);
-        $("#currentAddress").setValue("razDvaTrirazDvaTrirazDvaTrirazDvaTrirazDvaTrirazDvaTrirazDvaTrirazDvaTrirazDvaTrirazDvaTri");
+        $("#currentAddress").setValue("SomeText");
         $("#state").shouldBe(visible).click();
         $(byText("NCR")).click();
         $("#city").shouldBe(visible).click();
         $(byText("Noida")).click();
         $("#submit").click();
+
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+
+        $$(".table-responsive tbody tr").filterBy(text("Student Name")).first().shouldHave(text("Semen Petrovich"));
+        $$(".table-responsive tbody tr").filterBy(text("Student Email")).first().shouldHave(text("mblo@email.com"));
+        $$(".table-responsive tbody tr").filterBy(text("Gender")).first().shouldHave(text("Male"));
+        $$(".table-responsive tbody tr").filterBy(text("Mobile")).first().shouldHave(text("1234567890"));
+        $$(".table-responsive tbody tr").filterBy(text("Date of Birth")).first().shouldHave(text("27 December,1992"));
+        $$(".table-responsive tbody tr").filterBy(text("Subjects")).first().shouldHave(text("English"));
+        $$(".table-responsive tbody tr").filterBy(text("Hobbies")).first().shouldHave(text("Reading"));
+        $$(".table-responsive tbody tr").filterBy(text("Picture")).first().shouldHave(text("picture.png"));
+        $$(".table-responsive tbody tr").filterBy(text("Address")).first().shouldHave(text("SomeText"));
+        $$(".table-responsive tbody tr").filterBy(text("State and City")).first().shouldHave(text("NCR Noida"));
+
     }
 }
